9b5f74fa7f9feb533d79532b17bfe334efc42332
JangoJet <GeoffreyNorton111@outlook.com>
2021-03-16 21:30:14 +0000

    feat: Add audit feature to audit change and creation dates

--- src/VSTrain.Core/Common/Base.cs
+++ src/VSTrain.Core/Common/Base.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace VSTrain.Core.Common
 {
     public class Base
     {
         public int Id { get; set; }
+        public DateTimeOffset CreatedOn {get;set;}
+        public DateTimeOffset? LastModifiedOn {get;set;}
     }
 }
--- src/VSTrain.Infrastructure/VSTrain.Persistence/VSTrainDbContext.cs
+++ src/VSTrain.Infrastructure/VSTrain.Persistence/VSTrainDbContext.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using VSTrain.Core.Common;
 using VSTrain.Core.Entities;
 
 namespace VSTrain.Persistence
@@ -7,20 +11,38 @@ namespace VSTrain.Persistence
     {
         public VSTrainDbContext(DbContextOptions<VSTrainDbContext> options) : base(options)
         {
-            
+
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder){
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(VSTrainDbContext).Assembly);
             modelBuilder.Entity<Course>().HasData(
-                                                  new Course(){Id=1, Name="Scrum for Product Owners"},
-                                                  new Course(){Id=2, Name="ASP.NET Core 6.0"}
+                                                  new Course() { Id = 1, Name = "Scrum for Product Owners" },
+                                                  new Course() { Id = 2, Name = "ASP.NET Core 6.0" }
                                                   );
 
         }
-    public DbSet<Course> Courses;
-    
+        public DbSet<Course> Courses;
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach(var entry in ChangeTracker.Entries<Base>())
+            {
+                switch(entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedOn = DateTimeOffset.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedOn = DateTimeOffset.Now;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
     }
 
 }
